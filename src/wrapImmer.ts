d63acf93463b7334856af280f300672a0c4e1a07
takefumi-yoshii <takefumi.yoshii@dena.jp>
2018-07-12 01:16:09 +0900

    fix type guard

--- src/wrapImmer.ts
+++ src/wrapImmer.ts
@@ -1,9 +1,9 @@
 import immer from 'immer'
-import { KeyMap, Mutations, WrapImmerReturn } from '../typings'
+import { KeyMap, MutableMutations, ImmutableMutations } from '../typings'
 
-function wrapImmer<M extends KeyMap & Mutations<M, void>>(
+function wrapImmer<M extends KeyMap & MutableMutations<M>>(
   imutations: M
-): WrapImmerReturn<M> {
+): ImmutableMutations<M> {
   const mutations: KeyMap = {}
   Object.keys(imutations).forEach(imutationKey => {
     mutations[imutationKey] = <ST, PL>(state: ST, payload?: PL): ST =>
@@ -11,6 +11,6 @@ function wrapImmer<M extends KeyMap & Mutations<M, void>>(
         imutations[imutationKey](_state, payload)
       })
   })
-  return mutations as WrapImmerReturn<M>
+  return mutations as ImmutableMutations<M>
 }
 export { wrapImmer, immer }
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,16 +1,19 @@
 import immer from 'immer'
 
+type R<T> = T extends (...rest: any[]) => infer I ? I : never
 type A1<T> = T extends (a1: infer I, ...rest: any[]) => any ? I : never
 type A2<T> = T extends (a1: any, a2: infer I, ...rest: any[]) => any ? I : never
 type KeyMap = { [K: string]: any }
-type MT<T, R> = (state: A1<T>) => R
-type MTPL<T, R> = (state: A1<T>, payload: A2<T>) => R
-type Mutation<T, R> = MT<T, R> | MTPL<T, R>
-type Mutations<T, R> = { readonly [K in keyof T]: Mutation<T[K], R> }
-type ImmerWrapped<T> = T extends MT<T, void> ? MT<T, A1<T>> : MTPL<T, A1<T>>
-type WrapImmerReturn<T> = { readonly [K in keyof T]: ImmerWrapped<T[K]> }
-declare function wrapImmer<M extends KeyMap & Mutations<M, void>>(
+type MT<T> = R<T> extends void ? (state: A1<T>) => void : never
+type MTPL<T> = R<T> extends void ? (state: A1<T>, payload: A2<T>) => void : never
+type IMT<T> = (state: A1<T>) => A1<T>
+type IMTPL<T> = (state: A1<T>, payload: A2<T>) => A1<T>
+type MutableMutation<T> = MT<T> | MTPL<T>
+type MutableMutations<T> = { readonly [K in keyof T]: MutableMutation<T[K]> }
+type ImmutableMutation<T> = T extends MT<T> ? IMT<T> : T extends MTPL<T> ? IMTPL<T> : never
+type ImmutableMutations<T> = { readonly [K in keyof T]: ImmutableMutation<T[K]> }
+declare function wrapImmer<M extends KeyMap & MutableMutations<M>>(
   imutations: M
-): WrapImmerReturn<M>
+): ImmutableMutations<M>
 
-export { KeyMap, Mutations, WrapImmerReturn, wrapImmer, immer }
+export { KeyMap, MutableMutations, ImmutableMutations, wrapImmer, immer }
